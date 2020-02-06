5d04e7d711c19793ab2ed82326f007f5d3b86d86
tanakashun <michael021212@gmail.com>
2020-02-06 23:00:23 +0900

    seedにuser登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "y2@y", name: "yy", birthday: "1950-01-01", sex: "女", password: "000000", password_confirmation: "000000")
+User.create(email: "u2@u", name: "uu", birthday: "1960-01-01", sex: "男", password: "000000", password_confirmation: "000000")
+User.create(email: "i2@i", name: "ii", birthday: "1970-01-01", sex: "女", password: "000000", password_confirmation: "000000")
+User.create(email: "o2@o", name: "oo", birthday: "1980-01-01", sex: "男", password: "000000", password_confirmation: "000000")
+User.create(email: "p2@p", name: "pp", birthday: "1990-01-01", sex: "女", password: "000000", password_confirmation: "000000")
