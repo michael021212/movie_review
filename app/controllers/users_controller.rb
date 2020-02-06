5f3cd3e833d432b64c1e01af593ff332e627b35f
tanakashun <michael021212@gmail.com>
2020-02-07 00:21:53 +0900

    各モデルの基本的なCRUD機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -11,9 +12,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/update'
-  get 'reviews/destroy'
   get 'home/top'
   get 'home/about'
   root 'movies#index'
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   resources :users, only: [:index, :show, :edit, :update]
   resources :movies, only: [:index, :show]
+  resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
