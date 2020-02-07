93488e4984085654fbe15ca17c4eab6402378461
tanakashun <michael021212@gmail.com>
2020-02-08 00:00:06 +0900

    [update]movie一覧と詳細

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,17 +31,19 @@ API_KEY = "3a2bad199fe90fb84b52bdca279bf1bb";
 //   });
 // });
 
-fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=3a2bad199fe90fb84b52bdca279bf1bb&language=en-US&page=1')
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=3a2bad199fe90fb84b52bdca279bf1bb&language=en-US&page=1')
   .then(response => {
     return response.json();
   })
   .then(data => {
     //取得したJSONデータの処理
-    console.log(data);
+    console.log(data); //cosoleに表示(なくてもok)
+
     data.results.map(movie => {
       container = document.getElementById('app');
-      const row = document.createElement('div');
+      const row = document.createElement('a');
       row.setAttribute('class', 'item');
+      row.setAttribute('href', `/movies/${movie.id}`);
 
       const title = document.createElement('p');
       title.setAttribute('class', 'title');
@@ -52,6 +54,8 @@ fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=3a2bad199fe90fb84b
       genre_ids.textContent = movie.genre_ids;
 
       const poster = document.createElement('img');
+      poster.setAttribute('width', '100%');
+      poster.setAttribute('alt', 'サンプル');
       poster.src = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`;
 
       container.appendChild(row);
@@ -65,3 +69,55 @@ fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=3a2bad199fe90fb84b
     console.log('error');
   });
 
+// URLパラメータ文字列を取得する
+// var param = location.search
+// console.log(param);
+
+
+// function getId(ele){
+//   var id_value = ele.id; // eleのプロパティとしてidを取得
+//   console.log(id_value); //「id01」
+// }
+
+// movie_id = document.getElementById('767');
+// movie_id = 767
+movie_id = gon.movie_id //moviesコントローラーで定義した変数を代入
+fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=3a2bad199fe90fb84b52bdca279bf1bb&language=en-US`)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+
+    // ルーティングで定義したmoviesコントローラにapiのdataを:contentとして送る
+    // $.ajax({
+    //   url: "/movies/movie_data",
+    //   type: "GET",
+    //   data: {content : data},
+    //   datatype: "html",
+    // });
+
+    container = document.getElementById(movie_id);
+    const row = document.createElement('div');
+    row.setAttribute('class', 'item');
+
+    const title = document.createElement('p');
+    title.setAttribute('class', 'title');
+    title.textContent = data.title;
+
+    const genres = document.createElement('p');
+    genres.setAttribute('class', 'genres');
+    genres.textContent = data.genres;
+
+    const poster = document.createElement('img');
+    poster.src = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/${data.poster_path}`;
+
+    container.appendChild(row);
+    row.appendChild(poster);
+    row.appendChild(title);
+    row.appendChild(genres);
+  })
+  .catch(error => {
+    //エラー発生時の処理
+    console.log('error');
+  })
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,5 +3,13 @@ class MoviesController < ApplicationController
   end
 
   def show
+    @movie_id = params[:id]
+    gon.movie_id = params[:id]
+  end
+
+  # ajaxで送られたdataの処理
+  def movie_data
+    @content = params[:content]
+    @title = @content[:title]
   end
 end
