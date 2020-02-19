a2f9247877e352cf2951af9eeee9548964ad4d22
tanakashun <michael021212@gmail.com>
2020-02-19 14:28:53 +0900

    [add]goodの保存時にbadをdestroyするようにした。badも同様にした

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -2,6 +2,8 @@ class GoodsController < ApplicationController
   def create
     good = current_user.goods.build(review_id: params[:review_id])
     good.save
+    bad = Bad.find_by(user_id: current_user.id, review_id: params[:review_id])
+    bad.destroy
     redirect_to request.referer
   end
 
