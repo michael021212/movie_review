c0fa1f7159380adad5a1be162cd4099ceff1119b
tanakashun <michael021212@gmail.com>
2020-02-19 18:49:41 +0900

    [fix]interestの保存時のparamsを追加

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,6 +1,6 @@
 class InterestsController < ApplicationController
   def create
-    interest = current_user.interests.build(movie_id: params[:movie_id])
+    interest = current_user.interests.build(movie_id: params[:movie_id], title: params[:title], poster_path: params[:poster_path])
     interest.save
     redirect_to request.referer
   end
