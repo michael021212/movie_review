98922c2d4ad6682b77576d9532ecafaa84ee0894
tanakashun <michael021212@gmail.com>
2020-02-22 01:57:27 +0900

    [add]タイムライン

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def top
+    if user_signed_in?
+      @timeline_items = current_user.timeline.page(params[:page]).reverse_order
+    end
   end
 
   def about
